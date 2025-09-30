4ce02a8f4998f1e5fc6dfc69f09ca3d7b0b4fa94
Jordan Tobin <jordantobin123@hotmail.com>
2025-09-29 21:10:46 -0700

    Adding finish line functionality.

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // stuff happen. 
+        Debug.Log("FInish Is YOU.!"); 
+    }
+}
