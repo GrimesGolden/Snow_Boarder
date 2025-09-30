656748e78ac77f6a2440dac86204b6bec3689f47
Jordan Tobin <jmtobin@Jordans-MacBook-Air.local>
2025-09-30 16:49:44 -0700

    Refactor and serialize

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,12 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    int count = 0; 
+    [SerializeField] float loadDelay = 0.5f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Terrain")
         {
-            SceneManager.LoadScene(0); 
+            Invoke("ReloadScene", loadDelay); 
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
