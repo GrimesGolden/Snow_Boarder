4c110365c4bd316cfe24c5de5f6f858b3b7b423c
Jordan Tobin <jordantobin123@hotmail.com>
2025-10-10 21:38:06 -0700

    Added particle effects for finish, and crash. needs tuning

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,11 +4,12 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
-
+    [SerializeField] ParticleSystem crashEffect; 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Terrain")
         {
+            crashEffect.Play(); 
             Invoke("ReloadScene", loadDelay); 
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,12 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] float loadDelay = 1.0f;
+    [SerializeField] ParticleSystem finishEffect; 
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            finishEffect.Play(); 
             Invoke("ReloadScene", loadDelay);
         }
     }
