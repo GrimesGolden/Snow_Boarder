7be47f9b2859696d46969750e42816ae2c5ba957
Jordan Tobin <jordantobin123@hotmail.com>
2025-10-14 15:59:12 -0700

    Added sound effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,11 +4,14 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
-    [SerializeField] ParticleSystem crashEffect; 
+    [SerializeField] ParticleSystem crashEffect;
+    AudioSource crashSound; 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Terrain")
         {
+            crashSound = GetComponent<AudioSource>();
+            crashSound.Play(); 
             crashEffect.Play(); 
             Invoke("ReloadScene", loadDelay); 
         }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,12 +4,15 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1.0f;
-    [SerializeField] ParticleSystem finishEffect; 
+    [SerializeField] ParticleSystem finishEffect;
+    AudioSource finishSound; 
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            finishSound = GetComponent<AudioSource>();
+            finishSound.Play(); 
             finishEffect.Play(); 
             Invoke("ReloadScene", loadDelay);
         }
