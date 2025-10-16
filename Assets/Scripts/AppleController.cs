0b9e4a891b10b45c4e063f8a83ff6d025665d2b7
Jordan Tobin <jordantobin123@hotmail.com>
2025-10-16 14:30:46 -0700

    Added a rudimentery score, working on a boost

--- Assets/Scripts/AppleController.cs
+++ Assets/Scripts/AppleController.cs
@@ -2,16 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class AppleController : MonoBehaviour
 {
     [SerializeField] ParticleSystem appleEffect;
+    ScoreCounter scoreCounter;
+    float appleDelay = 0.5f; // This works, even if 0.5f is a bit too long. 
+    bool appleHit = false; 
+
+    void Start()
+    {
+        // Find a GameObject named ScoreCounter in the scene hierarchy.
+        GameObject scoreTxt = GameObject.Find("ScoreText");
+
+        // Get the ScoreCounter (Script) component out of scoreGO
+        scoreCounter = scoreTxt.GetComponent<ScoreCounter>();
+    }
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !appleHit)
         {
+            SoundManager.StopSound(); 
             SoundManager.PlaySound(SoundType.APPLE);
             appleEffect.Play();
-            Invoke("DestroyApple", 0.5f); 
+            scoreCounter.score += 1;
+            appleHit = true; 
+            Invoke("DestroyApple", appleDelay); 
       
         }
     }
---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class ScoreCounter : MonoBehaviour
+{
+    private TextMeshProUGUI uiText;
+    public int score = 0; 
+    void Start()
+    {
+        uiText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiText.text = "SCORE: " + score.ToString("#,0");
+    }
+}
