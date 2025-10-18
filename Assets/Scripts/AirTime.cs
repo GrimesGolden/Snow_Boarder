93e3b13ee89c790f17520b5d92cdaa03781157ab
Jordan Tobin <jordantobin123@hotmail.com>
2025-10-18 12:35:36 -0700

    Refactored AirTime script

--- Assets/Scripts/AirTime.cs
+++ Assets/Scripts/AirTime.cs
@@ -5,15 +5,16 @@ using Cinemachine;
 public class AirTime : MonoBehaviour
 {
 
-    // A script to handle the cameras zoom effect, zooming out when in the air for a long time.  
+    // A script to handle the cameras zoom effect: zooming out when Player in the air for a long time.  
 
-       CinemachineVirtualCamera vcam;
-    //[SerializeField] AudioClip slideSFX; 
+    CinemachineVirtualCamera vcam;
+    
     float t;
+    float standardCam = 10f; // The distance the camera will normally stand away from the player. 
 
-    float standardCam = 10f;
+    bool inAir = false; // Several bools for tracking air time.
     bool isLongAir = false;
-    //bool isMediumAir = false;  
+    bool isMediumAir = false; 
      void Start()
     {
         // Upon script start, find the virtual camera, and zero out the timer. 
@@ -22,68 +23,67 @@ public class AirTime : MonoBehaviour
     }
 
     void Update()
-    {
-        TrackAirTime(); 
+    {   
+        if(inAir)
+        {
+            // Only track when in air, this saves us updating every frame for no reason.
+            TrackAirTime();
+        }
     }
     
-    void TrackAirTime()
-    {   
-        // Some constants for camera values and timing. 
-        const int long_air = 5;
-        const int medium_air = 3;
-       // const int short_air = 2;
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        ResetAirTime();
+    }
 
-        const float long_cam = 30f;
+    void OnCollisionExit2D(Collision2D other)
+    {
+        // Reset the bool. 
+        inAir = true;
+        // Update will handle tracking. 
+    }
+
+    void TrackAirTime()
+    {
+        // Some constants for camera values and timing. 
+        const int long_air = 5; // The times to wait before zooming out. 
+        const int medium_air = 3;
+
+        const float long_cam = 30f; // The distance the camera will zoom. 
         const float medium_cam = 20f;
-        //const float short_cam = 15f;  
 
         // Continuously update time (it only resets upon a collision)
         // Essentially we are tracking how long "ducky" is in the air.
         t += Time.deltaTime;
 
         // Manage accordingly
-        if (t >= long_air)
+        if (t >= long_air && !isLongAir)
         {
             // The longer the time in air, the larger the camera will zoom out, for a cool effect. 
-            vcam.m_Lens.OrthographicSize = long_cam; 
-
-            if(!isLongAir)
-            {
-                SoundManager.PlaySound(SoundType.AIR);
-                isLongAir = true; 
-                // Set a air bool so the clip doesn't constantly play while in air. 
-            }
+            // It only triggers once. 
+            vcam.m_Lens.OrthographicSize = long_cam;
+            isLongAir = true;
+            inAir = false; // If long air is achieved, there is no reason to track anything else. Act as if we hit the ground.  
         }
-        else if (t >= medium_air)
+        else if (t >= medium_air && !isMediumAir)
         {
+            SoundManager.PlaySound(SoundType.AIR); // Play a sound here.
+            // Again this only triggers once. 
             vcam.m_Lens.OrthographicSize = medium_cam;
-
-           // if(!isMediumAir)
-          //  {
-             // /  SoundManager.PlaySound(SoundType.AIR);
-             //   isMediumAir = true; 
-          //  }
+            isMediumAir = true;
         }
-       // else if (t >= short_air)
-        //{
-       //     vcam.m_Lens.OrthographicSize = short_cam;
-       // }
     }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        ResetAirTime(); 
-    }
-
+    
     public void ResetAirTime()
     {   
         // Zero out the timer, and return to standard camera size. 
         t = 0;
         // Reset boolean triggers (these prevent looping sound)
+        inAir = false; 
         isLongAir = false;
-        //isMediumAir = false;
+        isMediumAir = false;
         // Return to the normal camera upon landing. 
         vcam.m_Lens.OrthographicSize = standardCam;
-
     }
 }
