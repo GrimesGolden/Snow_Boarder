92009846f5e565e863a57ff0890d0f8ee4e80959
Jordan Tobin <jordantobin123@hotmail.com>
2025-10-24 21:28:19 -0700

    MLG mode unlocked.

---
+++ Assets/Scripts/PopupText.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro; // omit if using legacy UI.Text
+
+public class PopupText : MonoBehaviour
+{
+    [SerializeField] float duration = 2f;   // seconds on screen
+    TextMeshProUGUI text;                   // or UnityEngine.UI.Text if using legacy
+    float timer;
+
+    void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        gameObject.SetActive(false); // could be false by default. 
+
+        Show("Thanks for playing ducky's world - Jordan ");
+    }
+
+    public void Show(string message)
+    {
+        text.text = message;
+        gameObject.SetActive(true);
+        timer = duration;
+    }
+
+    void Update()
+    {
+        if (!gameObject.activeSelf) return;
+
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
